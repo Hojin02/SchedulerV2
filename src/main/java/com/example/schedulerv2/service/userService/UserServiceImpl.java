37d95201a7d214e34cf7dfa09a002a0fd9eaca47
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2024-12-17 14:50:20 +0900

    ✨ Feat: Add login feature with email and password validation

--- src/main/java/com/example/schedulerv2/service/userService/UserServiceImpl.java
+++ src/main/java/com/example/schedulerv2/service/userService/UserServiceImpl.java
@@ -1,34 +1,54 @@
 package com.example.schedulerv2.service.userService;
 
+import com.example.schedulerv2.dto.userDto.UserLoginRequestDto;
 import com.example.schedulerv2.dto.userDto.UserRequestDto;
 import com.example.schedulerv2.dto.userDto.UserResponseDto;
 import com.example.schedulerv2.entity.User;
 import com.example.schedulerv2.repository.userRepository.UserRepository;
 import jakarta.persistence.EntityManager;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final EntityManager em;
+    private final HttpSession session;
+
     @Override
     public UserResponseDto addUser(UserRequestDto dto) {
         User user = userRepository.save(
-                new User(dto.getUserName(),dto.getEmail(), dto.getPassword())
+                new User(dto.getUserName(), dto.getEmail(), dto.getPassword())
         );
         return UserResponseDto.toDto(user);
     }
 
     @Override
+    public void loginUser(UserLoginRequestDto dto) {
+        Optional<User> optionalUser = userRepository.findByEmail(dto.getEmail());
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if (user.getPassword().equals(dto.getPassword())) {
+                session.setAttribute("userEmail",user.getEmail());
+            }
+        }else{
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Email or password does not match.");
+        }
+    }
+
+    @Override
     public List<UserResponseDto> findAllUser() {
-        List<User> users= userRepository.findAll();
-        return  users.stream()
+        List<User> users = userRepository.findAll();
+        return users.stream()
                 .map(UserResponseDto::toDto)
                 .collect(Collectors.toList());
 
@@ -44,7 +64,7 @@ public class UserServiceImpl implements UserService{
     @Override
     public UserResponseDto modifyUserById(Long id, UserRequestDto dto) {
         User user = userRepository.findByIdOrElseThrow(id);
-        user.updateUserNameAndEmail(dto.getUserName(),dto.getEmail());
+        user.updateUserNameAndEmail(dto.getUserName(), dto.getEmail());
         em.flush();
         return UserResponseDto.toDto(user);
     }
@@ -54,4 +74,5 @@ public class UserServiceImpl implements UserService{
         User user = userRepository.findByIdOrElseThrow(id);
         userRepository.delete(user);
     }
+
 }
